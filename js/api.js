8008d8745cbefd33744362b619600ef5743bc5e2
CorinaTalledo <corina.talledo@gmail.com>
2023-10-26 18:02:52 -0300

    ultimo commit

--- js/api.js
+++ js/api.js
@@ -1,6 +1,4 @@
 
-// La funcion que lleva info a la api debe recibir un parametro. Al mismo tiempo, la funcion del main tiene llamar a la funcion que lleva la info a la api y el parametro que va a recibir ahi es la que despues va a llevar a la api.
-
 const getJobs = async () => {
 
     showView("spinner")
@@ -15,7 +13,7 @@ const getJobs = async () => {
     inicializar(data);
     }, 2000);
 
-    getFilterOptions(data);
+    fillFilterArrays(data);
 };
 
 
@@ -39,12 +37,33 @@ const addJob = async (job) => {
         }
 
     } catch (error) {
-        alert(`Hubo un error`)
+        alert(`There was a mistake ${error}`)
     }
 };
 
 
-const seeDetail = async (id) => {
+const getFilterParams = async (key, value) => {
+
+    showView("spinner")
+
+    try{
+        let response = await fetch(`https://65209d90906e276284c4940c.mockapi.io/api/jobs/?${key}=${value}`)
+
+        if (response) {
+            let data = await response.json();
+
+            setTimeout(() => {
+                renderJobs(data)
+            }, 2000);
+
+        }
+    } catch (error) {
+        alert(`There was a mistake ${error}`);
+    }
+}
+
+
+const getDetail = async (id) => {
 
     showView("spinner")
 
@@ -54,33 +73,8 @@ const seeDetail = async (id) => {
 
 
     setTimeout(() => {
-        jobDetails(data);
+        renderDetails(data);
     }, 2000);
-}
-
-
-const getFilterParams = async (key, value) => {
-
-    showView("spinner")
-
-    // La informacion que toma es la correcta y la devolucion es correcta pero no lo puedo mostrar en el fucking html
-
-    try{
-        let response = await fetch(`https://65209d90906e276284c4940c.mockapi.io/api/jobs/?${key}=${value}`)
-
-        if (response) {
-            let data = await response.json();
-            
-            setTimeout(() => {
-                // showView("result")
-                getJobs();
-            }, 2000);
-
-            // console.log(data);
-        }
-    } catch (error) {
-        alert(`Hubo un error`);
-    }
 }
 
 
@@ -94,19 +88,19 @@ const deleteJob = async (id) => {
             },
         });
 
-        if (response.ok) {
-            alert(`El objeto ${id} ha sido eliminado The job offer ..... has been deleted.`)
+        if (response) {
+            alert(`The job offer ${id} has been deleted.`)
 
             getJobs()
         }
 
     } catch (error) {
-        alert(`Hubo un error`);
+        alert(`There was a mistake ${error}`);
     }
 }
 
 
-const seeEditForm = async (id) => {
+const getEditJob = async (id) => {
 
     showView("spinner")
 
@@ -114,19 +108,21 @@ const seeEditForm = async (id) => {
     
     let data = await response.json();
 
-
     setTimeout(() => {
-        seeForm(data);
+        showEditForm(data);
     }, 2000);
 }
 
 
 const editJob = async (id) => {
     try {
+        
+        const formData = editForm()
+
         const response = await fetch(`https://65209d90906e276284c4940c.mockapi.io/api/jobs/${id}`,
             {
             method: "PUT",
-            body: JSON.stringify(id),
+            body: JSON.stringify(formData),
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
             },
@@ -135,55 +131,12 @@ const editJob = async (id) => {
 
         const data = await response.json();
 
-        console.log(data);
-
         if(data){
-            editForm()
-            // getJobs();
+            alert(`The job offer ${id} has been edited.`)
+            getJobs();
         }
 
     } catch (error) {
-        alert(`Hubo un error`)
+        alert(`There was a mistake ${error}`)
     }
 };
-
-
-
-// fetch >> va a la api y el metodo por defecto, el verbo o metodo es get al menos que le digamos otro verbo.
-
-// FUNCION PARA TRAER LOS TRABAJOS DE LA API (SE LLAMA CADA VEZ QUE SE VA A ACTUALIZAR EL HTML)
-// FUNCION PARA AGREGAR LOS TRABAJOS (METODO POST)
-// FUNCION PARA ELIMINAR (delete)
-// FUNCION PARA ACTUALIZAR/EDITAR (patch)
-// funcion para filtrar los filtros. Solo debe mostrarse en el home con todas las cards.
-
-
-
-// VISTAS: (Funcion que muestra y oculta tiene que ir en otro doc: dom.js)
-// - Empleos disponibles
-// - Crear un trabajo
-// - Editar un trabajo
-
-// El spinner debe llamarse primero adentro de la funcion y el set time out abajo de todo pero dentro de la funcion
-
-
-
-
-//////////////////////////////// PREGUNTAS //////////////////////////////// 
-
-// - Al final tiene que haber un tercer documento para el dom o no?
-// - Como hacer para renderizar los trabajos filtrados.
-// - Como pasar el nombre del trabajo en vez del id en el alert al eliminarlo.
-// - El boton de editar puede abrir una nueva vista en vez de que este abajo del recuadro con la info acotada?
-// - Esta bien que aunque el cartel tome la info correcta no se actualice el url?
-
-
-
-
-
-
-
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 
-
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
-
 
 const showView = (vistaAMostrar) => {
     $$(".view").forEach((view) => view.classList.add("visually-hidden"));
     $(`#${vistaAMostrar}`).classList.remove("visually-hidden");
 };
 
+
+//------------------------------------------------
+//                BOTONES NAVBAR
+//------------------------------------------------
+
 $("#btn-jobs").addEventListener("click", () =>
     showView("section-jobs")
 );
 
+
 $("#show-form").addEventListener("click", () =>
-    showView("section-add-job")
+    showView("section-add-job"),
+    $("#add-thumbnail").style.backgroundImage = `url("https://ambientesdigital.com/wp-content/uploads/2013/03/250x250.jpg")`
 );
 
+
+
+//------------------------------------------------
+//                    INICIALIZAR
 //------------------------------------------------
 
 const inicializar = (data) =>{
@@ -24,12 +33,82 @@ const inicializar = (data) =>{
 }
 
 
-// FILTROS
-// Llena el primer select
-const getFilter = (data, filter) => {
-    const arrayOfOptions = [];
 
-    console.log(arrayOfOptions);
+//------------------------------------------------
+//            FUNCIONES QUE SE REUTILIZAN
+//------------------------------------------------
+
+// IMAGEN: Completa el contenedor de la imagen segun el valor del input
+const fillThumbnails = (idUrl, idThumbnail) =>{
+    if($(`#${idUrl}`).value){
+        $(`#${idThumbnail}`).style.backgroundImage = `url(${$(`#${idUrl}`).value})`
+    } else {
+        $(`#${idThumbnail}`).style.backgroundImage = `url("https://ambientesdigital.com/wp-content/uploads/2013/03/250x250.jpg")`
+    }
+}
+
+
+// SKILLS:
+
+let arraySkills = []
+
+
+// Funcionalidad del input para crear el array
+const inputSkill = (inputId, listId) =>{
+
+    $(`#${inputId}`).addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Permite que el enter no baje de linea
+            
+            const newSkill = $(`#${inputId}`).value.charAt(0).toUpperCase() + $(`#${inputId}`).value.slice(1);
+    
+            if (newSkill && arraySkills.length < 8) {
+                arraySkills.push(newSkill);
+                showSkill(listId);
+                $(`#${inputId}`).value = "";
+            }
+        }
+    });
+}
+
+
+// Permite visualizar cada elemento del array por separado
+const showSkill = (listId) => {
+
+    $(`#${listId}`).innerHTML = ""
+
+    arraySkills.forEach((skill) => {
+        $(`#${listId}`).innerHTML += `
+        <li class="list-group-item badge text-bg-dark d-flex justify-content-between align-items-center">
+            <span>${skill}</span>
+            <a class="btn btn-outline-dark d-flex align-items-center justify-content-center p-0" onclick="deleteSkill('${skill}', '${listId}')">
+                <i class="fa-regular fa-circle-xmark"></i>
+            </a>    
+        </li>`;
+    });
+}
+
+
+// Elimina el elemento tanto del array como de la pantalla
+const deleteSkill = (skill, listId) =>{
+    const index = arraySkills.indexOf(skill); // Busca el índice de la skill en el array
+    if (index !== -1) {
+        arraySkills.splice(index, 1); // Elimina el skill del array si se encuentra
+        showSkill(listId); // Actualiza la vista de la lista
+    }
+}
+
+
+
+//------------------------------------------------
+//                  FILTROS
+//------------------------------------------------
+
+//PRIMER SELECT: 
+
+//Completa el array de opciones y las muestra en el primer select
+const firstSelectFilter = (data, filter) => {
+    const arrayOfOptions = [];
 
     data.forEach((job) => {
         const value = job[filter];
@@ -44,8 +123,6 @@ const getFilter = (data, filter) => {
     arrayOfOptions.forEach((option) => {
         $("#select-option-filter").innerHTML += `<option>${option}</option>`;
     });
-
-    console.log(arrayOfOptions);
 }
 
 
@@ -58,14 +135,16 @@ $("#select-filter").addEventListener("change", () => {
 })
 
 
+// SEGUNDO SELECT:
+
 // Arrays globales que se van a llenar con todas las opciones
 const locations = [];
 const categories = [];
 const seniorities = [];
 
 
-// Toma las opciones para llenar los arrays
-const getFilterOptions = (data) => {
+// Toma las opciones para llenar los arrays (segundo select)
+const fillFilterArrays = (data) => {
 
     data.forEach((job) => {
         if(!locations.includes(job.location)) {
@@ -85,7 +164,6 @@ const getFilterOptions = (data) => {
 
 // Muestra las opciones en el segundo select
 const renderFilterOptions = (filterType) =>{
-    
     if(filterType === "location"){
         $("#select-option-filter").innerHTML = "";
         $("#select-option-filter").innerHTML = "<option selected>Select a Location</option>";
@@ -108,11 +186,12 @@ const renderFilterOptions = (filterType) =>{
             $("#select-option-filter").innerHTML += `<option>${seniority}</option>`;
         });
     } else {
-        $("#select-option-filter").innerHTML = `<option selected>Select a filter</option>`;
+        $("#select-option-filter").innerHTML = `<option>Select a Filter</option>`;
     }
 }
 
 
+// Unifica los values de los selects para ir a buscar a la API
 const renderFilter = () =>{
     let key = $("#select-filter").value
     let value = $("#select-option-filter").value
@@ -120,29 +199,37 @@ const renderFilter = () =>{
     getFilterParams(key, value);
 }
 
-
+// Boton para buscar el filtro seleccionado
 $("#btn-search").addEventListener("click", () => renderFilter())
 
 
+// Boton para limpiar lo seleccionado en los filtros
+$("#btn-clear").addEventListener("click", () => getJobs())
 
-// MOSTRAR TRABAJOS
+
+
+//------------------------------------------------
+//      VISUALIZACION DE LOS TRABAJOS CREADOS
+//------------------------------------------------
 
 const renderJobs = (jobs) =>{
 
     showView("section-jobs")
 
+    $("#cards-jobs").innerHTML = "";
+
     for (let { name, description, category, seniority, location, id } of jobs) {
         $("#cards-jobs").innerHTML +=
-            `<div class="card p-2 g-col-4" style="width: 300px; height: 300px; id="${id}">
+            `<div class="card p-2 g-col-4 shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 300px; height: 320px; id="${id}">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">${name}</h5>
                     <p class="job-description card-text">${description}</p>
                     <div class="mb-3">
-                        <span class="badge text-bg-primary">${location}</span>
-                        <span class="badge text-bg-primary">${category}</span>
-                        <span class="badge text-bg-primary">${seniority}</span>
+                        <span class="badge text-bg-success">${location}</span>
+                        <span class="badge text-bg-success">${category}</span>
+                        <span class="badge text-bg-success">${seniority}</span>
                     </div>
-                    <button type="button" class="btn btn-dark" onclick="seeDetail('${id}')">See Details</button>
+                    <button type="button" class="btn btn-dark" onclick="getDetail('${id}')">See Details</button>
                 </div>
             </div>`;   
     }
@@ -150,9 +237,13 @@ const renderJobs = (jobs) =>{
 
 
 
-// CREAR TRABAJOS
+//------------------------------------------------
+//                CREAR TRABAJOS
+//------------------------------------------------
 
+// Crea el objeto
 const createJob = () => {
+
     let newJob = {
         name: $("#add-job-title").value,
         image: $("#add-img").value,
@@ -167,50 +258,84 @@ const createJob = () => {
         },
         salary: Number($("#add-salary").value),
         long_term: $("#add-long-term").checked,
-        languages: $("#add-languages").value
+        skills: arraySkills
         }
 
     addJob(newJob)
 }
 
-$("#btn-add-job").addEventListener("click", () => createJob())
+
+// EN EL FORMULARIO:
+
+// Ejecuta la funcion que completa la img segun el link del input
+$("#add-img").addEventListener('input', ()=> fillThumbnails("add-img", "add-thumbnail"));
 
 
+// Ejecuta la funcion que completa y muestra el array de skills
+inputSkill("add-skills", "add-skills-list")
 
-// VER DETALES DEL TRABAJO SELECCIONADO
 
-const jobDetails = ({image, name, description, category, seniority, location, id}) =>{
+//Boton para enviar el formulario
+$("#btn-add-job").addEventListener("click", (e) => {
+    e.preventDefault() // Evita que se envie sin guardar la info
+    createJob() // Crea el trabajo
+    resetForm() // Resetea todo el formulario
+})
 
-    showView("section-detail")
 
-    $("#section-detail").innerHTML = `
-        <div class="card mb-3" style="max-width: 50%;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="${image}" class="img-fluid rounded-start">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title" id="detail-name">${name}</h5>
-                                <p class="card-text" id="detail-description">${description}</p>
-                                <div class="mb-3">
-                                    <span class="badge text-bg-primary" id="detail-location">${location}</span>
-                                    <span class="badge text-bg-primary" id="detail-category">${category}</span>
-                                    <span class="badge text-bg-primary" id="detail-seniority">${seniority}</span>
-                                </div>
+// Funcion para resetar todo el formulario
+const resetForm = () =>{
+    $(`#${"add-thumbnail"}`).style.backgroundImage = `url("https://ambientesdigital.com/wp-content/uploads/2013/03/250x250.jpg")`;
 
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <button type="button" class="btn btn-success" onclick="seeEditForm('${id}')"><i class="fa-solid fa-pencil me-2"></i>Edit</button>
-                                    <button type="button" class="btn btn-danger"  id="btn-delete-job" onclick="removeJob('${id}')"><i class="fa-solid fa-trash-can me-2"></i>Delete</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+    $(`#${"add-skills-list"}`).innerHTML = "";
+
+    $(`#${"add-job-form"}`).reset()
 }
 
 
 
+//------------------------------------------------
+//               VISTA VER DETALLES
+//------------------------------------------------
+
+
+// Tarjeta del trabajo seleccionado
+const renderDetails = ({image, name, description, category, seniority, location, id}) =>{
+
+    showView("section-detail")
+    
+    $("#section-detail").innerHTML = `
+        <div class="card mb-3 m-auto w-50" style="height: 290px;">
+            <div class="row g-0 h-100">
+                <div class="col-md-4 h-100">
+                    <img src="${image}" class="thumbnail img-fluid rounded-start" id="detail-thumbnail" onerror="this.src='https://ambientesdigital.com/wp-content/uploads/2013/03/250x250.jpg'">
+                </div>
+                <div class="col-md-8 h-100">
+                    <div class="card-body h-100 d-flex flex-column justify-content-between">
+                        <div>
+                            <h5 class="card-title" id="detail-name">${name}</h5>
+                            <p class="card-text job-description" id="detail-description">${description}</p>
+                        </div>
+                        <div>
+                            <div class="mb-3">
+                                <span class="badge text-bg-success" id="detail-location">${location}</span>
+                                <span class="badge text-bg-success" id="detail-category">${category}</span>
+                                <span class="badge text-bg-success" id="detail-seniority">${seniority}</span>
+                            </div>
+
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button type="button" class="btn btn-success" onclick="getEditJob('${id}')"><i class="fa-solid fa-pencil me-2"></i>Edit</button>
+                                <button type="button" class="btn btn-danger"  id="btn-delete-job" onclick="removeJob('${id}')"><i class="fa-solid fa-trash-can me-2"></i>Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
+
+
+// Funcionalidad del boton "Delete"
 const removeJob = (id) => {
 
     const confirmDelete = confirm("Are you sure you want to delete this job offer?");
@@ -221,13 +346,18 @@ const removeJob = (id) => {
     }
 }
 
-$("#btn-delete-job").addEventListener("click", () => removeJob(id))
 
 
+//------------------------------------------------
+//            FORMULARIO EDITAR TRABAJO
+//------------------------------------------------
 
-const seeForm = ({name, description, image, location, seniority, category, long_term, salary, benefits, languages}) =>{
+// Llena el formualrio con la info del trabajo
+const showEditForm = ({name, description, image, location, seniority, category, long_term, salary, benefits, skills, id}) =>{
 
-    showView("section-edit-job")
+    showView("section-edit-job"),
+
+    $("#edit-job-thumbnail").style.backgroundImage = `url(${image})`
 
     $("#edit-job-name").value = name;
     $("#edit-job-description").value = description;
@@ -240,14 +370,27 @@ const seeForm = ({name, description, image, location, seniority, category, long_
     $("#edit-job-vacation").value = benefits.vacation;
     $("#edit-job-health").value = benefits.health_ensurance;
     $("#edit-job-internet").checked = benefits.internet_paid;
-    $("#edit-job-languages").value = languages;
+    $("#edit-job-skills").value = "";
 
-    $("#btn-edit-job").addEventListener("click", () => editForm())
+    arraySkills = skills || [];
+
+    showSkill("edit-skills-list")
+
+    id = id    
+
+    $("#btn-edit-job").onclick = (e) => {
+        e.preventDefault();
+        editJob(id)
+    } 
 }
 
+// Ejecuta la funcion para visualizar y crear el array de skills
+inputSkill("edit-job-skills", "edit-skills-list")
 
+
+// Permite cambiar la info del objeto
 const editForm = () =>{
-    return{
+    let formData = {
         name: $("#edit-job-name").value,
         description: $("#edit-job-description").value,
         image: $("#edit-job-img").value,
@@ -261,26 +404,14 @@ const editForm = () =>{
             health_ensurance: $("#edit-job-health").value,
             internet_paid: $("#edit-job-internet").checked,
         },
-        languages: $("#edit-job-languages").value,
+        skills: arraySkills,
     }
+
+    return formData
 }
 
-
-
-
-// El add event listener se activa al hacer click en el boton de editar trabajo.
-// Tengo que capturar la informacion de ese trabajo trayendola de la api con un fetch.
-// Tengo que meter esa informacion en el formulario.
-
-// Tengo que poder modificar esa informacion
-// Tengo que poder guardarla y editarla en la api.
-
-
-
-
-
-
-// []
+// Permite modificar el enlace de la foto y que se visualice en el thumnail
+$("#edit-job-img").addEventListener('input', ()=> fillThumbnails("edit-job-img", "edit-job-thumbnail"));
 
 
 
